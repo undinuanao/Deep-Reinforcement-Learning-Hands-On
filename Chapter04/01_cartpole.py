bb51ac2a0223bebad6d25a993d4160d23a0039c9
sundingyuanhao <1320047710@njupt.edu.cn>
2021-06-30 10:52:05 +0800

    增加了yield速度测试的实验，yield真的好快

--- Chapter04/01_cartpole.py
+++ Chapter04/01_cartpole.py
@@ -40,14 +40,14 @@ def iterate_batches(env, net, batch_size):
     while True:
         obs_v = torch.FloatTensor([obs])
         # print("obs_v:", obs_v)
-        act_prob_test = net(obs_v)
+        # act_prob_test = net(obs_v)
         # print("未经过Softmax处理过的网络输出：",act_prob_test)
         act_probs_v = sm(net(obs_v))
         # print("Softmax处理后的网络输出:", act_probs_v)
-        act_probs = act_probs_v.data.numpy()[0]
-        # print("act_probs:", act_probs)
+        act_probs = act_probs_v.data.numpy()[0] #将tensor类型的张量转换为numpy里的ndarray类型
+        # print("动作的概率act_probs是：{0}, 其数据类型是：{1}".format(act_probs, type(act_probs)))
         action = np.random.choice(len(act_probs), p=act_probs)
-        # print("ACTION = ", action)
+        # print("选择的动作ACTION：", action)
         next_obs, reward, is_done, _ = env.step(action)
         episode_reward += reward
         episode_steps.append(EpisodeStep(observation=obs, action=action))
@@ -64,11 +64,11 @@ def iterate_batches(env, net, batch_size):
 
 def filter_batch(batch, percentile):
     rewards = list(map(lambda s: s.reward, batch))
-    print("所有的奖励为：", rewards)
+    # print("所有的奖励为：{0},共有{1}个".format(rewards,len(rewards)))
     reward_bound = np.percentile(rewards, percentile)
-    print("奖励边界为：", reward_bound)
+    # print("奖励边界为：", reward_bound)
     reward_mean = float(np.mean(rewards))
-    print("奖励均值为：", reward_mean)
+    # print("奖励均值为：", reward_mean)
 
     train_obs = []
     train_act = []
@@ -76,38 +76,41 @@ def filter_batch(batch, percentile):
         if example.reward < reward_bound:
             continue
         train_obs.extend(map(lambda step: step.observation, example.steps))
+        # print("样本中的观察train_obs为:",train_obs[0::20])
         train_act.extend(map(lambda step: step.action, example.steps))
-
+        # print("样本中的动作train_act为:",train_act[0::20])
     train_obs_v = torch.FloatTensor(train_obs)
     train_act_v = torch.LongTensor(train_act)
     return train_obs_v, train_act_v, reward_bound, reward_mean
 
 
 if __name__ == "__main__":
-    env = gym.make("CartPole-v0")
-    # env = gym.wrappers.Monitor(env, directory="mon", force=True)
-    obs_size = env.observation_space.shape[0]
-    n_actions = env.action_space.n
+    for i in range(10):
+        env = gym.make("CartPole-v0")
+        # env = gym.wrappers.Monitor(env, directory="mon", force=True)
+        obs_size = env.observation_space.shape[0]
+        n_actions = env.action_space.n
 
-    net = Net(obs_size, HIDDEN_SIZE, n_actions)
-    objective = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(params=net.parameters(), lr=0.01)
-    writer = SummaryWriter(comment="-cartpole")
+        net = Net(obs_size, HIDDEN_SIZE, n_actions)
+        objective = nn.CrossEntropyLoss()
+        optimizer = optim.Adam(params=net.parameters(), lr=0.01)
+        writer = SummaryWriter(comment="-cartpole")
 
-    for iter_no, batch in enumerate(iterate_batches(env, net, BATCH_SIZE)):
-        obs_v, acts_v, reward_b, reward_m = filter_batch(batch, PERCENTILE)
-        optimizer.zero_grad()
-        action_scores_v = net(obs_v)
-        loss_v = objective(action_scores_v, acts_v)
-        loss_v.backward()
-        # env.render()
-        optimizer.step()
-        # print("%d: loss=%.3f, reward_mean=%.1f, reward_bound=%.1f" % (
-            # iter_no, loss_v.item(), reward_m, reward_b))
-        writer.add_scalar("loss", loss_v.item(), iter_no)
-        writer.add_scalar("reward_bound", reward_b, iter_no)
-        writer.add_scalar("reward_mean", reward_m, iter_no)
-        if reward_m > 199:
-            print("Solved!")
-            break
-    writer.close()
+    
+        for iter_no, batch in enumerate(iterate_batches(env, net, BATCH_SIZE)):
+            obs_v, acts_v, reward_b, reward_m = filter_batch(batch, PERCENTILE)
+            optimizer.zero_grad()
+            action_scores_v = net(obs_v)
+            loss_v = objective(action_scores_v, acts_v)
+            loss_v.backward()
+            # env.render()
+            optimizer.step()
+            print("%d: loss=%.3f, reward_mean=%.1f, reward_bound=%.1f" % (
+                iter_no, loss_v.item(), reward_m, reward_b))
+            writer.add_scalar("loss", loss_v.item(), iter_no)
+            writer.add_scalar("reward_bound", reward_b, iter_no)
+            writer.add_scalar("reward_mean", reward_m, iter_no)
+            if reward_m > 199:
+                print("Solved!")
+                break
+        writer.close()
---
+++ yield_exp_2.py
@@ -0,0 +1,46 @@
+import sys
+import time 
+
+def fab_memory(max): 
+   n, a, b = 0, 0, 1 
+   L = [] 
+   while n < max: 
+       L.append(b) 
+       a, b = b, a + b 
+       n = n + 1 
+   return L
+
+def fab_yield(max): 
+    n, a, b = 0, 0, 1 
+    while n < max: 
+        yield b 
+        # print b 
+        a, b = b, a + b 
+        n = n + 1
+
+rounds = 100000
+
+start = time.perf_counter_ns()
+fibonacci = fab_memory(rounds)
+end = time.perf_counter_ns()
+print('内存消耗型共用时:', end - start)
+
+f = iter(fab_memory(rounds))
+while True:
+    start = time.perf_counter_ns()
+    try:
+        next(f)
+    except StopIteration:
+        end = time.perf_counter_ns()
+        print('迭代器与生成器共用时:', end - start)
+        break
+
+x = fab_yield(rounds)
+while True:
+    start = time.perf_counter_ns()
+    try:
+        next(x)
+    except:
+        end = time.perf_counter_ns()
+        print('使用yield共用时:', end - start)
+        break
